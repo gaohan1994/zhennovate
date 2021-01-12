00965c43f385dbc5f747e0cda71a5e102ad2ccaf
gaohan1994 <871418277@qq.com>
2021-01-12 15:12:10 +0800

    feat(insights): 新增insights表头
    新增表头，新增点击表头切换表格内容

--- src/pages/index/insights/component/chart.jsx
+++ src/pages/index/insights/component/chart.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import echarts from 'echarts/lib/echarts';
 import 'echarts/lib/chart/line';
 import 'echarts/lib/chart/bar';
@@ -7,114 +7,132 @@ import 'echarts/lib/component/toolbox';
 import 'echarts/lib/component/tooltip';
 import 'echarts/lib/component/dataZoom';
 import 'echarts/lib/component/axis';
+import { random } from 'lodash';
+
+/**
+ * 针对传入的数据给出echarts的option
+ */
+const setEchartsOptions = (data) => {
+  const getDataNumber = (length) => {
+    const array = new Array(length);
+    for (let i = 0; i < array.length; i++) {
+      const value = random(0, 50);
+      array[i] = value;
+    }
+    return array;
+  };
+
+  return {
+    tooltip: {
+      trigger: 'axis',
+    },
+    legend: {
+      icon: 'rect',
+      itemWidth: 12,
+      itemHeight: 12,
+      itemGap: 16,
+      bottom: 0,
+      data: ['Concerned', 'Angry', 'Stressed', 'Anxious'],
+    },
+    grid: {
+      top: '3%',
+      left: '0%',
+      right: '2%',
+      bottom: '14%',
+      containLabel: true,
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: [
+        'Nov 1',
+        'Nov 2',
+        'Nov 3',
+        'Nov 4',
+        'Nov 5',
+        'Nov 6',
+        'Nov 7',
+        'Nov 8',
+        'Nov 9',
+        'Nov 10',
+        'Nov 11',
+        'Nov 12',
+        'Nov 13',
+        'Nov 14',
+        'Nov 15',
+      ],
+    },
+    yAxis: {
+      type: 'value',
+      show: true,
+      axisLine: {
+        show: true,
+      },
+    },
+    series: [
+      {
+        name: 'Concerned',
+        type: 'line',
+        symbol: 'circle',
+        symbolSize: 8,
+        data: getDataNumber(15),
+      },
+      {
+        name: 'Angry',
+        type: 'line',
+        symbol: 'circle',
+        symbolSize: 8,
+        data: getDataNumber(15),
+      },
+      {
+        name: 'Stressed',
+        type: 'line',
+        symbol: 'circle',
+        symbolSize: 8,
+        data: getDataNumber(15),
+      },
+      // {
+      //   name: 'Anxious',
+      //   type: 'line',
+      //   stack: '总量',
+      //   symbol: 'circle',
+      //   symbolSize: 8,
+      //   data: [320, 332, 301, 334, 390, 330, 320],
+      // },
+    ],
+  };
+};
 
 function InsightChart(props) {
   const { data = {} } = props;
 
-  /**
-   * 针对传入的数据给出echarts的option
-   */
-  const setEchartsOptions = (data) => {
-    return {
-      tooltip: {
-        trigger: 'axis',
-      },
-      legend: {
-        icon: 'rect',
-        itemWidth: 12,
-        itemHeight: 12,
-        itemGap: 16,
-        bottom: 0,
-        data: ['Concerned', 'Angry', 'Stressed', 'Anxious'],
-      },
-      grid: {
-        top: '3%',
-        left: '0%',
-        right: '2%',
-        bottom: '14%',
-        containLabel: true,
-      },
-      xAxis: {
-        type: 'category',
-        boundaryGap: false,
-        data: [
-          'Nov 1',
-          'Nov 2',
-          'Nov 3',
-          'Nov 4',
-          'Nov 5',
-          'Nov 6',
-          'Nov 7',
-          'Nov 8',
-          'Nov 9',
-          'Nov 10',
-          'Nov 11',
-          'Nov 12',
-          'Nov 13',
-          'Nov 14',
-          'Nov 15',
-        ],
-      },
-      yAxis: {
-        type: 'value',
-        show: true,
-        axisLine: {
-          show: true,
-        },
-      },
-      series: [
-        {
-          name: 'Concerned',
-          type: 'line',
-          symbol: 'circle',
-          symbolSize: 8,
-          data: [0, 20, 20, 40, 60, 0, 0, 0, 60, 50, 0, 10, 20, 40, 50],
-        },
-        {
-          name: 'Angry',
-          type: 'line',
-          symbol: 'circle',
-          symbolSize: 8,
-          data: [60, 40, 0, 0, 0, 0, 0, 20, 40, 40, 20, 0, 0, 10, 10],
-        },
-        {
-          name: 'Stressed',
-          type: 'line',
-          symbol: 'circle',
-          symbolSize: 8,
-          data: [90, 80, 40, 60, 70, 60, 80, 60, 80, 90, 60, 50, 40, 30, 0],
-        },
-        // {
-        //   name: 'Anxious',
-        //   type: 'line',
-        //   stack: '总量',
-        //   symbol: 'circle',
-        //   symbolSize: 8,
-        //   data: [320, 332, 301, 334, 390, 330, 320],
-        // },
-      ],
-    };
-  };
-
+  const [myCharts, setMyCharts] = useState({});
   /**
    * 初始化echarts
    */
   useEffect(() => {
-    let myChart = echarts.getInstanceByDom(
+    let mc = echarts.getInstanceByDom(
       document.getElementById('insight-charts'),
     );
-    if (myChart === undefined) {
-      myChart = echarts.init(document.getElementById('insight-charts'));
+    if (mc === undefined) {
+      mc = echarts.init(document.getElementById('insight-charts'));
     }
-    myChart.setOption(setEchartsOptions(data));
+    mc.setOption(setEchartsOptions(data));
+    setMyCharts(mc);
 
     window.addEventListener('resize', function () {
-      myChart.resize();
+      mc.resize();
     });
     return () => {
       window.removeEventListener('resize', () => {});
     };
   }, []);
+
+  useEffect(() => {
+    if (myCharts.setOption) {
+      myCharts.setOption(setEchartsOptions(data));
+    }
+  }, [data, myCharts]);
 
   return <div id="insight-charts" style={{ width: '100%', height: 420 }} />;
 }
--- src/pages/index/insights/emotion/index.jsx
+++ src/pages/index/insights/emotion/index.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../index.less';
 import Sort from '@/component/sort';
 import InsightChart from '../component/chart';
+import { ArrowUpOutlined } from '@ant-design/icons';
 
 const prefix = 'page-insights';
 
+const data = [
+  { title: 'Mad', value: 36, data: 1 },
+  { title: 'Calm', value: 39, data: 10 },
+  { title: 'Brave', value: 13, data: 20 },
+  { title: 'Sad', value: 42, data: 30 },
+];
+
 function Emotion() {
+  const [activeTitle, setActiveTitle] = useState(data[0]);
+
+  const onClickTab = (item) => {
+    setActiveTitle(item);
+  };
+
   return (
     <div>
       <Sort title="Understanding your emotions" showSort={false} />
@@ -16,7 +30,38 @@ function Emotion() {
           options={[{ label: 'Last 14 days', value: '1' }]}
         />
 
-        <InsightChart />
+        <section className={`${prefix}-emotion`}>
+          <section>
+            {data.map((item) => {
+              const active = item.title === activeTitle.title;
+              return (
+                <div
+                  className={`${prefix}-emotion-item`}
+                  key={item.title}
+                  onClick={() => onClickTab(item)}
+                >
+                  <span title-active={active ? 'active' : ''}>
+                    {item.title}
+                  </span>
+                  <h1 title-active={active ? 'active' : ''}>{item.value}</h1>
+                  <div
+                    className={`${prefix}-emotion-item-border`}
+                    title-active={active ? 'active' : ''}
+                  />
+                </div>
+              );
+            })}
+            <div className={`${prefix}-emotion-border`} />
+          </section>
+
+          <div className={`${prefix}-emotion-tip`}>
+            <div>
+              <ArrowUpOutlined style={{ color: '#fff', fontSize: 12 }} />
+            </div>
+            Mad emotions increased by 20% in the last 14 days.
+          </div>
+        </section>
+        <InsightChart data={activeTitle.data} />
       </div>
     </div>
   );
